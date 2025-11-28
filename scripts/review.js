efc08d77650564939574eb6bdab6f086d5a36104
EmerRPereira <epereira3@byupathway.edu>
2025-11-28 00:44:20 -0300

    Translate text

--- scripts/review.js
+++ scripts/review.js
@@ -1,8 +1,8 @@
-// Contador de avaliações via localStorage
+// Rating counter via localStorage
 let count = Number(localStorage.getItem("reviewCount")) || 0;
 count++;
 localStorage.setItem("reviewCount", count);
 
 
-// Exibe no HTML
+// Display in HTML
 document.getElementById("counter").textContent = count;
